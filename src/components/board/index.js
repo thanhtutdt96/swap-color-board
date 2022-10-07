e0aef73b1bcea0bff95804a418edd0dc3379b518
Tu Pham <thanhtutdt96@gmail.com>
2022-10-07 18:38:34 +0700

    Update code

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -10,8 +10,10 @@ const Board = () => {
         <div className="Board">
             <h1>Color Board</h1>
             <p>*** Drag and drop a box to another on the board in order to swap them ***</p>
-            <div className="Board__table">
-                {colorBoard.map((item) => <Box key={item.id} item={item}/>)}
+            <div className="Board__table-wrapper">
+                <div className="Board__table">
+                    {colorBoard.map((item) => <Box key={item.id} item={item}/>)}
+                </div>
             </div>
         </div>
     )
